daee0f1848761e153cdfa0d6fd6c173c88d2afa7
Claudia Augusta do carmo <71201080+Parvita@users.noreply.github.com>
2021-04-15 15:57:25 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+function myFunction() {
+   document.getElementById("demo").innerHTML = "Show Roamap";
+
+
+ }
+
+ function school() {
+   document.getElementById("a").innerHTML = "Schools return to live teaching";
+ }
+
+
+ function outdoor() {
+   document.getElementById("b").innerHTML = "Outdoor gatherings will return, limited to 6 people/2 households";
+ }
+
+ function nonEssential() {
+   document.getElementById("c").innerHTML = "Non-essential retail, gyms and hairdressers reopen";
+ }
+
+ function gatherings() {
+   document.getElementById("d").innerHTML = "Outdoor gatherings increased to 30 people,movies to reopen";
+ }
+
+ function lifted() {
+   document.getElementById("e").innerHTML = "All restrictions lifted";
+ }
+
+    // <!-- The <button> tag defines a clickable button. -->
+    // <!-- "document.getElementById finds the element with the id -->
+    // <!-- The static Date.now() method returns the number of milliseconds elapsed since January 1, 1970 00:00:00 UTC. -->
+
+
